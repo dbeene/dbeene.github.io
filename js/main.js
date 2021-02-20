5794dc3acdc88f1a10d75521c78e0bd8a02690e1
Daniel Beene <57543540+dbeene@users.noreply.github.com>
2021-02-19 20:22:58 -0700

    Alpha version of website w/ research activities in animated panels

--- js/main.js
+++ js/main.js
@@ -130,3 +130,134 @@ $(document).ready(function () {
     });
 });
 
+///// RESEARCH BOX 4 /////
+//Display text using class selector "hoverable"
+$(document).ready(function () {
+
+    $("div .research-block4").mouseover(function () {
+        $(this).find(".hoverable").show();
+    });
+
+    $("div .research-block4").mouseout(function () {
+        $(this).find(".hoverable").hide();
+    });
+});
+
+//Change opacity @ mouseover
+$(document).ready(function () {
+    $(".research-block4").mouseover(function () {
+        $(".research-block1").css("opacity", "80%");
+    });
+    $(".research-block4").mouseout(function () {
+        $(".research-block4").css("opacity", "100%");
+    });
+});
+
+//Animate div @ mouseover
+var baseMargin = 5;
+$(document).ready(function () {
+    $(".research-block4").hover(function () {
+        $(this).stop().animate({ marginTop: (baseMargin + 5) + 'px' }, 500);
+    },
+        function () {
+            $(this).stop().animate({ marginTop: (baseMargin - 5) + 'px' }, 500);
+        });
+});
+
+//Add drop shadow @ mouseover
+$(document).ready(function () {
+    $(".research-block4").mouseover(function () {
+        $(".research-block4").css("box-shadow", "12px 20px 18px #3f474d"); 
+    });
+    $(".research-block4").mouseout(function () {
+        $(".research-block4").css("box-shadow", "none");
+    });
+});
+
+///// RESEARCH BOX 5 /////
+//Display text using class selector "hoverable"
+$(document).ready(function () {
+
+    $("div .research-block5").mouseover(function () {
+        $(this).find(".hoverable").show();
+    });
+
+    $("div .research-block5").mouseout(function () {
+        $(this).find(".hoverable").hide();
+    });
+});
+
+//Change opacity @ mouseover
+$(document).ready(function () {
+    $(".research-block5").mouseover(function () {
+        $(".research-block1").css("opacity", "80%");
+    });
+    $(".research-block5").mouseout(function () {
+        $(".research-block5").css("opacity", "100%");
+    });
+});
+
+//Animate div @ mouseover
+var baseMargin = 5;
+$(document).ready(function () {
+    $(".research-block5").hover(function () {
+        $(this).stop().animate({ marginTop: (baseMargin + 5) + 'px' }, 500);
+    },
+        function () {
+            $(this).stop().animate({ marginTop: (baseMargin - 5) + 'px' }, 500);
+        });
+});
+
+//Add drop shadow @ mouseover
+$(document).ready(function () {
+    $(".research-block5").mouseover(function () {
+        $(".research-block5").css("box-shadow", "12px 20px 18px #3f474d"); 
+    });
+    $(".research-block5").mouseout(function () {
+        $(".research-block5").css("box-shadow", "none");
+    });
+});
+
+///// RESEARCH BOX 6 /////
+//Display text using class selector "hoverable"
+$(document).ready(function () {
+
+    $("div .research-block6").mouseover(function () {
+        $(this).find(".hoverable").show();
+    });
+
+    $("div .research-block6").mouseout(function () {
+        $(this).find(".hoverable").hide();
+    });
+});
+
+//Change opacity @ mouseover
+$(document).ready(function () {
+    $(".research-block6").mouseover(function () {
+        $(".research-block1").css("opacity", "80%");
+    });
+    $(".research-block6").mouseout(function () {
+        $(".research-block6").css("opacity", "100%");
+    });
+});
+
+//Animate div @ mouseover
+var baseMargin = 5;
+$(document).ready(function () {
+    $(".research-block6").hover(function () {
+        $(this).stop().animate({ marginTop: (baseMargin + 5) + 'px' }, 500);
+    },
+        function () {
+            $(this).stop().animate({ marginTop: (baseMargin - 5) + 'px' }, 500);
+        });
+});
+
+//Add drop shadow @ mouseover
+$(document).ready(function () {
+    $(".research-block6").mouseover(function () {
+        $(".research-block6").css("box-shadow", "12px 20px 18px #3f474d"); 
+    });
+    $(".research-block6").mouseout(function () {
+        $(".research-block6").css("box-shadow", "none");
+    });
+});
